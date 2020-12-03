a022c0b56ba58ea5edd61db71119d7c0464bb194
masi846 <masii.shaneshin@gmail.com>
2020-12-02 20:33:35 -0500

    indext

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const { fetchMyIP } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
+  }
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
 
-  console.log('It worked! Returned IP:' , ip);
 });
