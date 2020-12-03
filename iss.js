50504b14b911efecdb5252348390bb201daae644
masi846 <masii.shaneshin@gmail.com>
2020-12-02 21:14:56 -0500

    add file

--- iss.js
+++ iss.js
@@ -8,18 +8,25 @@ const request = require('request');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const fetchMyIP = function(callback) {
-  request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) return callback(error, null);
-
-    if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
       return;
     }
 
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
   });
 };
-
-module.exports = { fetchMyIP };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  // ...
+};
+// Don't need to export the other function since we are not testing it right now.
+module.exports = { fetchCoordsByIP }
